66bc7569378be361844ebbf70a6b957da4a79a31
Ryan Tan <oathyu@gmail.com>
2023-11-30 15:36:30 +0800

    Getting Started

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,7 @@
+//Create a new cargo project with `cargo new`
+//Build with `cargo build` and build + run with `cargo run`
+//Built files are by default placed in the `target` folder
+//Compile without building an executable with `cargo check`
+fn main() {
+    println!("Hello, world!");
+}
---
+++ hello_world/main.rs
@@ -0,0 +1,6 @@
+//All programs' entry point is the main function
+//! means that you are calling a macro and not a function
+//.pdb file is a file containing debugging information
+fn main() {
+    println!("Hello world!")
+}
